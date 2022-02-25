0e184a3c3620186dacfe5401d0a699d6d73d2b71
Mau Olascoaga <mau.olascoaga@gmail.com>
2022-02-25 13:04:54 -0600

    clean up chapter

---
+++ src/components/TheNavegation.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="nav">
+    <router-link id="logo" to="/">Home</router-link>
+    <router-link
+      v-for="destination in destinations"
+      :key="destination.id"
+      :to="{
+        name: 'destination.show',
+        params: { id: destination.id, slug: destination.slug },
+      }"
+    >
+      {{ destination.name }}
+    </router-link>
+  </div>
+</template>
+
+<style lang="css">
+#nav .active-link {
+  color: white;
+  border-bottom: 1px solid brown;
+}
+</style>
+
+<script>
+import sourceData from "@/data.json";
+export default {
+  data() {
+    return {
+      destinations: sourceData.destinations,
+    };
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -8,19 +8,16 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/destination/:id/:slug",
+    name: "destination.show",
+    component: () => import("@/views/DepartmentsShow.vue"),
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkActiveClass: "active-link",
 });
 
 export default router;

de5dce73b33644b402d7531c20c560fe23068ce8
Mau Olascoaga <mau.olascoaga@gmail.com>
2022-03-01 10:58:04 -0600

    scroll control

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <TheNavegation />
   <div class="container">
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="slide" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -12,3 +16,15 @@ export default {
   components: { TheNavegation },
 };
 </script>
+
+<style lang="css">
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 0.25s, transform 0.25s;
+}
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
+}
+</style>
---
+++ src/views/DepaTemplate.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h1>{{ experience.name }}</h1>
+    <img :src="`/images/${experience.image}`" alt="destination.name" />
+    <p>{{ experience.description }}</p>
+    <GoBack />
+  </section>
+</template>
+
+<script>
+import sourceData from "@/data.json";
+import GoBack from "@/components/GoBack.vue";
+export default {
+  components: { GoBack },
+  props: {
+    id: { type: Number, required: true },
+    experienceSlug: { type: String, required: true },
+  },
+  computed: {
+    destination() {
+      return sourceData.destinations.find(
+        (destination) => destination.id === this.id
+      );
+    },
+    experience() {
+      return this.destination.experiences.find(
+        (experience) => experience.slug === this.experienceSlug
+      );
+    },
+  },
+};
+</script>
